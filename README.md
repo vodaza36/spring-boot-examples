4b91e0b7df1ba56798fd54719fe4e2291539c8b2
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-03 09:46:03 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# About this repo
+
+This repository contains Spring Boot examples.
+
+## Submodules
+- [Baeldung Testing in Spring Boot](https://www.baeldung.com/spring-boot-testing)
+
+ 
---
+++ baeldung-test/src/main/java/at/hochbichler/spring/baeldungtest/BaeldungTestApplication.java
@@ -0,0 +1,12 @@
+package at.hochbichler.spring.baeldungtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeldungTestApplication.class, args);
+    }
+}
